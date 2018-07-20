7790975c8e3db72b70cf7799cbc43c6319fabe32
Skyler Escobedo <skylerescobedo@gmail.com>
2018-07-20 16:53:05 -0700

    Fixed formatting

--- resolvers/user.js
+++ resolvers/user.js
@@ -3,8 +3,7 @@ import formatErrors from "./../formatErrors";
 
 export default {
   Query: {
-    getUser: (parent, { id }, { models }) =>
-      models.User.findOne({ where: { id } }),
+    getUser: (parent, { id }, { models }) => models.User.findOne({ where: { id } }),
     allUsers: (parent, args, { models }) => models.User.findAll()
   },
   Mutation: {
